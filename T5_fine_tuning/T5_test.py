f0a2e794e094890436efb5c49484feef2bb89b24
Lim_heejin <dg961108@naver.com>
2022-01-17 22:16:59 +0900

    Add files via upload

---
+++ T5_fine_tuning/T5_test.py
@@ -0,0 +1,34 @@
+from transformers import T5Config, T5Tokenizer, T5ForConditionalGeneration
+import torch
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+# fine-tuning model이 test 진행을 하기 위한 사용자 함수
+def test(epoch, tokenizer, model, device, loader):
+    model.eval()
+    predictions = []
+    actuals = []
+    with torch.no_grad():
+        for _, data in enumerate(loader, 0):
+            y = data['target_ids'].to(device, dtype = torch.long)
+            ids = data['source_ids'].to(device, dtype = torch.long)
+            mask = data['source_mask'].to(device, dtype = torch.long)
+
+            # 문장을 생성하는 generate 함수
+            generated_ids = model.generate(
+                input_ids = ids,
+                attention_mask = mask, 
+                max_length=150, 
+                num_beams=2,
+                repetition_penalty=2.5, 
+                length_penalty=1.0, 
+                early_stopping=True
+                )
+            preds = [tokenizer.decode(g, skip_special_tokens=True, clean_up_tokenization_spaces=True) for g in generated_ids]
+            target = [tokenizer.decode(t, skip_special_tokens=True, clean_up_tokenization_spaces=True)for t in y]
+            if _%100==0:
+                print(f'Completed {_}')
+
+            predictions.extend(preds)
+            actuals.extend(target)
+    return predictions, actuals
